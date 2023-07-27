8d221585c5c2970361f26dfcb0c8781c9338c159
Hyunji_ <hyunji1443@gmail.com>
2023-07-27 11:37:26 +0900

    noticeVO 다시 작성

--- src/main/java/com/javava/controller/NoticeController.java
+++ src/main/java/com/javava/controller/NoticeController.java
@@ -1,14 +1,11 @@
 package com.javava.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javava.service.NoticeService;
-import com.javava.vo.NoticeVO;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;  
